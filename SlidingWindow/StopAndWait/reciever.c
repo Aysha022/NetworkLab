b625900a7078388659c0755fe38224a720565e9d
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-04-06 23:54:57 +0530

    Update reciever.c

--- SlidingWindow/StopAndWait/reciever.c
+++ SlidingWindow/StopAndWait/reciever.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-#include<sys/socket.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
+#include<sys/socket.h>
 #include<arpa/inet.h>
 
 #define SA struct sockaddr
@@ -13,7 +13,7 @@
 int main()
 {
 	int sockfd,connfd,len,n,len1,count=0;
-	SAI serv_addr,cli_addr;
+	SAI server,client;
 	char buffer[MAX],buffer1[MAX]="ACKNOWLEDGEMENT",buffer2[MAX]="no";
 	
 	sockfd=socket(AF_INET,SOCK_STREAM,0);
@@ -22,11 +22,11 @@ int main()
 	serv_addr.sin_port = htons(PORT);
 	serv_addr.sin_addr.s_addr =INADDR_ANY;
 	
-	bind(sockfd,(SA*)&serv_addr,sizeof(serv_addr));
+	bind(sockfd,(SA*)&server,sizeof(server));
 	
 	listen(sockfd,5);
-	len = sizeof(cli_addr);
-    connfd=accept(sockfd,(struct sockaddr*)&cli_addr,(socklen_t*)&len);
+	len = sizeof(client);
+        connfd=accept(sockfd,(SA*)&client,(socklen_t*)&len);
 	
 	bzero(buffer,MAX);
 	read(connfd,buffer,MAX);
@@ -34,7 +34,7 @@ int main()
 	int no= atoi(buffer);
 	int i=0;
 	while(1){
-        bzero(buffer,MAX);
+                bzero(buffer,MAX);
 		read(connfd,buffer,MAX);
 		printf("the message is : %s\n",buffer);
 		if((strcmp("3",buffer)==0) || (strcmp("5",buffer)==0)){
