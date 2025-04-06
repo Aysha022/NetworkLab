d6372325fccc2fae5cf8eea94b80e345043966a2
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-04-06 23:41:05 +0530

    Update sender.c

--- SlidingWindow/StopAndWait/sender.c
+++ SlidingWindow/StopAndWait/sender.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
-#include<sys/socket.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
 #include<arpa/inet.h>
+#include<sys/socket.h>
+
 #define PORT 8080
 #define MAX 1000
 #define SA struct sockaddr
 #define SAI struct sockaddr_in
+
 int main(){
 	int sockfd,n=0;
+	SAI server;
 	char buffer[MAX];
-	SAI serv_addr;
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
 	
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
 	
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
@@ -21,21 +23,22 @@ int main(){
 	
 	bzero(buffer,MAX);
 	
-	if((connect(sockfd,(struct sockaddr*)& serv_addr,sizeof(serv_addr)))==0)
-        printf("Connected with server..\n");
+	if((connect(sockfd,(SA*)& server,sizeof(server)))==0)
+          printf("Connected with server..\n");
 	printf("Enter the no of message:");
 	while((buffer[n++]=getchar())!='\n');	
 	write(sockfd,buffer,MAX);
 	int no= atoi(buffer);
 	int i =0;
 	while(1){
-        bzero(buffer,MAX);	
+                bzero(buffer,MAX);	
 		printf("Enter the data: ");
-        n=0;
+                n=0;
 		while((buffer[n++]=getchar())!='\n');	
 		write(sockfd,buffer,MAX);
 		printf("the message is : %s\n",buffer);
-        bzero(buffer,MAX);
+		
+                bzero(buffer,MAX);
 		read(sockfd,buffer,MAX);
 		printf("the reply is : %s\n",buffer);
 		if(strcmp("ACKNOWLEDGEMENT",buffer)==0){
