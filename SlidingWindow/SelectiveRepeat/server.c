323d8a2cead29f7199f14f2456134b1192b51695
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-04-07 22:15:54 +0530

    Update server.c

--- SlidingWindow/SelectiveRepeat/server.c
+++ SlidingWindow/SelectiveRepeat/server.c
@@ -1,101 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<sys/time.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h> 
-#include<fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-
-void rsendd(int ch,int c_sock){
-char buff2[60];
-		bzero(buff2,sizeof(buff2));
-		strcpy(buff2,"reserver message :");
-		buff2[strlen(buff2)]=(ch)+'0';
-		buff2[strlen(buff2)]='\0';
-		printf("Resending Message to client :%s \n",buff2);
-		write(c_sock, buff2, sizeof(buff2));
-		usleep(1000);
+void resend_message(int id, int sock) {
+    char msg[50];
+    sprintf(msg, "server-msg-%d", id);
+    printf("Resending: %s\n", msg);
+    write(sock, msg, sizeof(msg));
 }
+
 int main() {
-	int s_sock, c_sock;
-	s_sock = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in server, other;
-	memset(&server, 0, sizeof(server));
-	memset(&other, 0, sizeof(other));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(9009);
-	server.sin_addr.s_addr = INADDR_ANY;
-	socklen_t add;
-	
+    int s_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server = {0}, client;
 
-	if(bind(s_sock, (struct sockaddr*)&server, sizeof(server)) == -1) {
-		printf("Binding failed\n");
-		return 0;
-	}
-	printf("\tServer Up\n Selective repeat scheme\n\n");
-	listen(s_sock, 10); 
-	add = sizeof(other);
-	c_sock = accept(s_sock, (struct sockaddr*)&other, &add);
-    time_t t1,t2;
-	char msg[50]="server message :";
-	char buff[50];
-    int flag=0;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(9009);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-fd_set set1,set2,set3;
-  struct timeval timeout1,timeout2,timeout3;
-  int rv1,rv2,rv3;
+    bind(s_sock, (struct sockaddr*)&server, sizeof(server));
+    listen(s_sock, 5);
 
-int tot=0;
-int ok[20];
-memset(ok,0,sizeof(ok));
+    socklen_t len = sizeof(client);
+    int c_sock = accept(s_sock, (struct sockaddr*)&client, &len);
 
-while(tot<9){
-int toti=tot;
-for(int j=(0+toti);j<(3+toti);j++){
-		//printf("%d %d %d \n",tot,toti,j);
-		bzero(buff,sizeof(buff));
-		char buff2[60];
-		bzero(buff2,sizeof(buff2));
-		strcpy(buff2,"server message :");
-		buff2[strlen(buff2)]=(j)+'0';
-		buff2[strlen(buff2)]='\0';
-		printf("Message sent to client :%s \n",buff2,tot,j);
-		write(c_sock, buff2, sizeof(buff2));
-		usleep(1000);
-}
-for(int k=0+toti;k<(toti+3);k++){
-		qq:
-		FD_ZERO(&set1); 
-  		FD_SET(c_sock, &set1);
-  		timeout1.tv_sec = 2;
-  		timeout1.tv_usec = 0;
-		
-        rv1 = select(c_sock + 1, &set1, NULL, NULL, &timeout1);
-  		if(rv1 == -1)
-    	perror("select error ");
-  		else if(rv1 == 0){
-      	printf("Timeout for message :%d \n",k);
-		rsendd(k,c_sock);
-		goto qq;} // a timeout occured 
-  		else{
-  		read(c_sock, buff, sizeof(buff));
-		printf("Message from Client: %s\n", buff);
-		if(buff[0]=='n'){
-		printf(" corrupt message awk (msg %d) \n",buff[strlen(buff)-1]-'0');
-		rsendd((buff[strlen(buff)-1]-'0'),c_sock);
-		goto qq;}
-		else
-		tot++;
-		//printf("%d %d %d \n",tot,toti,k);
-		}
-}
-}
-	
-	close(c_sock);
-	close(s_sock);
-	return 0;
+    printf("Server started...\n");
+
+    int base = 0;
+    int window_size = 3;
+    char buff[50];
+    int total_messages = 9;
+
+    while (base < total_messages) {
+        printf("\nSending Window (base = %d)\n", base);
+
+        // Send window of messages
+        for (int i = base; i < base + window_size && i < total_messages; i++) {
+            sprintf(buff, "server-msg-%d", i);
+            write(c_sock, buff, sizeof(buff));
+            printf("ent: %s\n", buff);
+        }
+
+        // Read ACKs or NACKs
+        for (int i = base; i < base + window_size && i < total_messages; i++) {
+            bzero(buff, sizeof(buff));
+            read(c_sock, buff, sizeof(buff));
+
+            int ack_id = buff[strlen(buff) - 1] - '0';
+
+            if (buff[0] == 'N') {
+                printf("NACK received for msg-%d\n", ack_id);
+                resend_message(ack_id, c_sock);
+                i--; // Retry this same message
+            } else {
+                printf("ACK received for msg-%d\n", ack_id);
+                base++;
+            }
+        }
+    }
+
+    printf("\nAll messages sent and acknowledged!\n");
+    close(c_sock);
+    close(s_sock);
+    return 0;
 }
