ac1fce29b002b60a1a7075116be93d2deb39247d
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-04-08 00:08:05 +0530

    Update server.c

--- SlidingWindow/SelectiveRepeat/server.c
+++ SlidingWindow/SelectiveRepeat/server.c
@@ -5,26 +5,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void resend_message(int id, int sock) {
+void resend_message(int id, int sockfd) {
     char msg[50];
     sprintf(msg, "server-msg-%d", id);
     printf("Resending: %s\n", msg);
-    write(sock, msg, sizeof(msg));
+    write(sockfd, msg, sizeof(msg));
 }
 
 int main() {
-    int s_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server = {0}, client;
 
     server.sin_family = AF_INET;
     server.sin_port = htons(9009);
-    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(s_sock, (struct sockaddr*)&server, sizeof(server));
-    listen(s_sock, 5);
+    bind(sockfd, (struct sockaddr*)&server, sizeof(server));
+    listen(sockfd, 5);
 
     socklen_t len = sizeof(client);
-    int c_sock = accept(s_sock, (struct sockaddr*)&client, &len);
+    int connfd = accept(sockfd, (struct sockaddr*)&client, &len);
 
     printf("Server started...\n");
 
@@ -39,20 +39,20 @@ int main() {
         // Send window of messages
         for (int i = base; i < base + window_size && i < total_messages; i++) {
             sprintf(buff, "server-msg-%d", i);
-            write(c_sock, buff, sizeof(buff));
-            printf("ent: %s\n", buff);
+            write(connfd, buff, sizeof(buff));
+            printf("Sent: %s\n", buff);
         }
 
         // Read ACKs or NACKs
         for (int i = base; i < base + window_size && i < total_messages; i++) {
             bzero(buff, sizeof(buff));
-            read(c_sock, buff, sizeof(buff));
+            read(connfd, buff, sizeof(buff));
 
             int ack_id = buff[strlen(buff) - 1] - '0';
 
             if (buff[0] == 'N') {
                 printf("NACK received for msg-%d\n", ack_id);
-                resend_message(ack_id, c_sock);
+                resend_message(ack_id, connfd);
                 i--; // Retry this same message
             } else {
                 printf("ACK received for msg-%d\n", ack_id);
@@ -62,7 +62,7 @@ int main() {
     }
 
     printf("\nAll messages sent and acknowledged!\n");
-    close(c_sock);
-    close(s_sock);
+    close(connfd);
+    close(sockfd);
     return 0;
 }
