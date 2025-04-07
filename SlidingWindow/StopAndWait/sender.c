446ab43e2fffb0f5026411320f55f6f0f1cbe50e
Aysha Naurin <149690596+Aysha022@users.noreply.github.com>
2025-04-07 12:16:38 +0530

    Update sender.c

--- SlidingWindow/StopAndWait/sender.c
+++ SlidingWindow/StopAndWait/sender.c
@@ -5,48 +5,54 @@
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
-#define PORT 8080
+#define PORT 8900
 #define MAX 1000
 #define SA struct sockaddr
 #define SAI struct sockaddr_in
 
 int main(){
-	int sockfd,n=0;
-	SAI server;
-	char buffer[MAX];
-	
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
-	
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-	serv_addr.sin_addr.s_addr =htonl(INADDR_ANY);
-	
-	bzero(buffer,MAX);
-	
-	if((connect(sockfd,(SA*)&server,sizeof(server)))==0)
+        int sockfd,n=0;
+        SAI server;
+        char buff[MAX];
+
+        sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+        server.sin_family = AF_INET;
+        server.sin_port = htons(PORT);
+        server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        //bzero(buffer,MAX);
+
+        if((connect(sockfd,(SA*)&server,sizeof(server))) == 0) {
           printf("Connected with server..\n");
-	printf("Enter the no of message:");
-	while((buffer[n++]=getchar())!='\n');	
-	write(sockfd,buffer,MAX);
-	int no = atoi(buffer);
-	int i = 0;
-	while(1){
-                bzero(buffer,MAX);	
-		printf("Enter the data: ");
-                n=0;
-		while((buffer[n++]=getchar())!='\n');	
-		write(sockfd,buffer,MAX);
-		printf("the message is : %s\n",buffer);
-		
-                bzero(buffer,MAX);
-		read(sockfd,buffer,MAX);
-		printf("the reply is : %s\n",buffer);
-		if(strcmp("ACKNOWLEDGEMENT",buffer)==0){
-			i++;	
-		}	
-		if(i==no){
-			break;
-		}
-	}
-    close(sockfd);	
+        }
+
+        bzero(buff,MAX);
+        printf("Enter the number of messages : ");
+        while((buff[n++] = getchar()) != '\n');
+        write(sockfd,buff,sizeof(buff));
+        int no = atoi(buff);
+        int i = 1;
+
+        while(1) {
+                bzero(buff,MAX);
+                printf("Enter the data : ");
+                n = 0;
+                while((buff[n++] = getchar()) != '\n');
+                write(sockfd,buff,sizeof(buff));
+                printf("The message is %s\n",buff);
+
+                bzero(buff,MAX);
+                read(sockfd,buff,sizeof(buff));
+                printf("The reply is %s\n",buff);
+
+                if(strcmp("ack",buff) == 0) {
+                   i++;
+                }
+
+                if(i == no) {
+                   break;
+                }
+        }
+        close(sockfd);
 }
